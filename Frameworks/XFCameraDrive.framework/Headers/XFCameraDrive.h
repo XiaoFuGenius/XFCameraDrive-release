a7081654c135c26e18b6be213d31499edd998b04
hu_wenfeng <hwf@xiaofutech.com>
2019-11-11 11:42:33 +0800

    v1.0.2

---
+++ Frameworks/XFCameraDrive.framework/Headers/XFCameraDrive.h
@@ -0,0 +1,33 @@
+//
+//  XFCameraDrive.h
+//  XFCameraDrive
+//
+//  Created by 胡文峰 on 2019/7/8.
+//  Copyright © 2019 xiaofutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XFCameraDrive.
+FOUNDATION_EXPORT double XFCameraDriveVersionNumber;
+
+//! Project version string for XFCameraDrive.
+FOUNDATION_EXPORT const unsigned char XFCameraDriveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XFCameraDrive/PublicHeader.h>
+
+/**
+ XFCameraDrive版本号
+ 更新时间：2019.11.10 20:13
+ */
+#define XFCameraDrive_SDK_VERSION @"1.0.2"
+
+#import "CTUVCHelper.h"
+
+
+/**
+XFCameraDrive Version History *
+ 
+ last update：2019.11.07 16:05   Version：1.0.1
+ last update：2019.09.20 12:35   Version：1.0.0
+*/
